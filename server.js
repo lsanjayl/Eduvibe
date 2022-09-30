21ea5bd33b79e5ea03ae8f880678e1b5f7a5a19c
sanjay <84283520+lsanjayl@users.noreply.github.com>
2022-09-30 20:44:55 +0530

    final 1

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ app.post('/contact',(req,res)=>{
     const transporter =nodemailer.createTransport({
         service:"gmail",
         auth:{
-            user:'sanjaygm744@gmail.com',
-            pass:'mnqdewjcfagbirku'
+            user:'jabez@creatorslab.in',
+            pass:'rjvfuraugidbfytm'
         }
     })
     const mailOptions = {
         from:req.body.email,
         to:'sec20it068@sairamtap.edu.in',
         subject:`Message from ${req.body.email}:${req.body.name}`,
-        text:req.body.message
+        html:`<p>Name:${req.body.name}</p><p>Phone:${req.body.phone}</p><p>Department:${req.body.department}</p><p>Collegename:${req.body.collegename}</p><p>Currentyear:${req.body.currentyear}</p><p>Message:${req.body.message}</p>`,
     }
     transporter.sendMail(mailOptions,(error,info)=>{
         if(error){
